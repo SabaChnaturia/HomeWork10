178f65a6fb1d716e2e6ddddd0c45327bd17ba27e
SabaChnaturia <154993776+SabaChnaturia@users.noreply.github.com>
2024-09-05 22:19:17 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let word = prompt("Enter word");
+if (word[0]==="A" || word[0]==="E" ||word[0]==="I" ||word[0]==="O"||word[0]==="U" ||word[0]==="a" || word[0]==="e" ||word[0]==="i" ||word[0]==="o"||word[0]==="u"){
+    console.log(`სიტყვა იწყება ხმოვანით რომელიც,არის ${word[0]}`)
+}
+else{
+    console.log(`ეს სიტყვა არ იწყება ხმოვნით, ${word[0]}`)
+}
